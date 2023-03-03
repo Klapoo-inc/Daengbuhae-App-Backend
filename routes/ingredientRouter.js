f8e78c3f586d61fb520da507932546489eb3b3e1
SeoYoungHong <stiokang2@dgist.ac.kr>
2023-03-03 09:30:54 +0900

    ingredientsearch

--- routes/ingredientRouter.js
+++ routes/ingredientRouter.js
@@ -4,5 +4,6 @@ const ingredientController = require('../controllers/ingredientController');
 const router = express.Router();
 
 router.post('/get', ingredientController.getIngredients);
+router.post('/search', ingredientController.searchIngredient);
 
 module.exports = router;
