b7391c7be25df523c2feae274e76cf90d67bc44d
SeoYoungHong <stiokang2@dgist.ac.kr>
2023-03-07 18:34:45 +0900

    cosmeticcreatedelete

--- controllers/cosmeticController.js
+++ controllers/cosmeticController.js
@@ -1,5 +1,7 @@
 const cosmeticDao = require('../daos/cosmeticDao');
 const cosmeticDto = require('../dtos/cosmeticDto');
+const petDto = require("../dtos/petDto");
+const petDao = require("../daos/petDao");
 
 /**
  * @swagger
@@ -104,4 +106,114 @@ const getCosmetic = async (req, res) => {
     }
 };
 
-module.exports = { searchCosmetics, getCosmetic };
+/**
+ * @swagger
+ * /cosmetic/create:
+ *   post:
+ *     summary: 화장품등록
+ *     tags:
+ *       - 화장품
+ *     description:
+ *       "Uid: 유저 id (필수) <br>
+ *       birth: 생일 (필수) <br>
+ *       gender: 성별 <br>
+ *       src: 이미지 <br>
+ *       kind: 종 <br>
+ *       name: 이름 <br>
+ *       neutering: 중성화 <br>
+ *       vaccination: 백신 <br>
+ *       weight: 무게 <br>
+ *       allergic: 알레르기 <br>
+ *       ill: 질병 <br>
+ *       bath: 목욕"
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *
+ *               Cid:
+ *                  type: integer
+ *               title:
+ *                 type: string
+ *               brand:
+ *                 type: string
+ *               SCategory:
+ *                  type: string
+ *               BCategory:
+ *                  type: string
+ *               src:
+ *                  type: string
+ *                  default: null
+ *               CoupangSrc:
+ *                  type: string
+ *                  default: null
+ *               NInhibition:
+ *                  type: boolean
+ *               NLimit:
+ *                  type: boolean
+ *               Allergic:
+ *                  type: boolean
+ *               PPH:
+ *                  type: boolean
+ *               CountFunctional:
+ *                  type: object
+ *               CountRating:
+ *                  type: object
+ *               CountColor:
+ *                  type: object
+ *
+ *
+ *     responses:
+ *       200:
+ *         description: Success
+ *       500:
+ *         description: Internal Server Error
+ */
+
+const createCosmetic = async (req, res) => {
+    try {
+        const request = cosmeticDto.fromRequest_create(req);
+        const pet = await cosmeticDao.Create(request);
+        res.status(200).json(pet);
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error', error });
+    }
+};
+
+/**
+ * @swagger
+ * /cosmetic:
+ *   delete:
+ *     summary: 화장품 삭제
+ *     tags:
+ *       - 화장품
+ *     description:
+ *       "Cid: 화장품 id (필수)"
+ *     parameters:
+ *       - name: Cid
+ *         description: 화장품 id
+ *         in: query
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: Success
+ *       500:
+ *         description: Internal Server Error
+ */
+
+const deleteCosmetic = async (req, res) => {
+    try {
+        const request = cosmeticDto.fromRequest_delete(req);
+        console.log(request, req.body)
+        const pet = await cosmeticDao.Delete(request.Cid);
+        res.status(200).json(pet);
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error', error });
+    }
+};
+
+module.exports = { searchCosmetics, getCosmetic, createCosmetic, deleteCosmetic };
