b9b89acaab55e5a1b3d156fa4c2b8cc2bbc88efd
SeoYoungHong <stiokang2@dgist.ac.kr>
2023-07-18 09:52:30 +0900

    'address'

--- models/payments/addressModel.js
+++ models/payments/addressModel.js
@@ -1,20 +1,36 @@
 const { Sequelize, sequelize } = require('../sequelize');
 
 const Address = sequelize.define('Address', {
-    Pid: {
+    Aid: {
         type: Sequelize.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true,
     },
-    read: {
+    Uid: {
+        type: Sequelize.STRING(128),
+        allowNull: false,
+    },
+    name: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+    },
+    address: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+    },
+    addressdetail: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+    },
+    postnum: {
         type: Sequelize.INTEGER,
         allowNull: false,
     },
-    src: {
-        type: Sequelize.STRING(255),
-        allowNull: true,
-    },
+    phonenumber: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    }
 }, {
     tableName: "Address",
     paranoid: true,
